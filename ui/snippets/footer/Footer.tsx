ee55431014bd3a818a0e5fa22eb347455746e298
a <a@vc.sb>
2024-06-25 21:52:00 +0800

    tg

--- ui/snippets/footer/Footer.tsx
+++ ui/snippets/footer/Footer.tsx
@@ -1,29 +1,36 @@
-import type { GridProps } from '@chakra-ui/react';
-import { Box, Grid, Flex, Text, Link, VStack, Skeleton } from '@chakra-ui/react';
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import type { GridProps } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  Flex,
+  Text,
+  Link,
+  VStack,
+  Skeleton,
+} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
-import type { CustomLinksGroup } from 'types/footerLinks';
+import type { CustomLinksGroup } from "types/footerLinks";
 
-import config from 'configs/app';
-import type { ResourceError } from 'lib/api/resources';
-import useApiQuery from 'lib/api/useApiQuery';
-import useFetch from 'lib/hooks/useFetch';
-import useIssueUrl from 'lib/hooks/useIssueUrl';
-import NetworkAddToWallet from 'ui/shared/NetworkAddToWallet';
+import config from "configs/app";
+import type { ResourceError } from "lib/api/resources";
+import useApiQuery from "lib/api/useApiQuery";
+import useFetch from "lib/hooks/useFetch";
+import useIssueUrl from "lib/hooks/useIssueUrl";
+import NetworkAddToWallet from "ui/shared/NetworkAddToWallet";
 
-import FooterLinkItem from './FooterLinkItem';
-import IntTxsIndexingStatus from './IntTxsIndexingStatus';
-import getApiVersionUrl from './utils/getApiVersionUrl';
+import FooterLinkItem from "./FooterLinkItem";
+import IntTxsIndexingStatus from "./IntTxsIndexingStatus";
+import getApiVersionUrl from "./utils/getApiVersionUrl";
 
 const MAX_LINKS_COLUMNS = 4;
 
-const FRONT_VERSION_URL = `https://github.com/blockscout/frontend/tree/${ config.UI.footer.frontendVersion }`;
-const FRONT_COMMIT_URL = `https://github.com/blockscout/frontend/commit/${ config.UI.footer.frontendCommit }`;
+const FRONT_VERSION_URL = `https://github.com/blockscout/frontend/tree/${config.UI.footer.frontendVersion}`;
+const FRONT_COMMIT_URL = `https://github.com/blockscout/frontend/commit/${config.UI.footer.frontendCommit}`;
 
 const Footer = () => {
-
-  const { data: backendVersionData } = useApiQuery('config_backend_version', {
+  const { data: backendVersionData } = useApiQuery("config_backend_version", {
     queryOptions: {
       staleTime: Infinity,
     },
@@ -44,22 +51,22 @@ const Footer = () => {
     //   url: 'https://blockscout.canny.io/feature-requests',
     // },
     {
-      icon: 'social/git' as const,
-      iconSize: '18px',
-      text: 'ZTC Chain',
-      url: 'https://github.com/ztcchain',
+      icon: "social/git" as const,
+      iconSize: "18px",
+      text: "ZTC Chain",
+      url: "https://github.com/ztcchain",
     },
     {
-      icon: 'social/twitter' as const,
-      iconSize: '18px',
-      text: 'X (ZTCGlobal)',
-      url: 'https://twitter.com/ZTCGlobal',
+      icon: "social/twitter" as const,
+      iconSize: "18px",
+      text: "X (ZTCGlobal)",
+      url: "https://twitter.com/ZTCGlobal",
     },
     {
-      icon: 'social/discord' as const,
-      iconSize: '24px',
-      text: 'Discord',
-      url: 'https://discord.gg/9yEB946CXE',
+      icon: "social/discord" as const,
+      iconSize: "24px",
+      text: "Discord",
+      url: "https://discord.gg/9yEB946CXE",
     },
     // {
     //   icon: 'discussions' as const,
@@ -75,20 +82,28 @@ const Footer = () => {
     // },
     // telegram
     {
-      icon: 'social/telega' as const,
-      iconSize: '20px',
-      text: 'Telegram',
-      url: 'https://t.me/ZTCCLASS',
+      icon: "social/telega" as const,
+      iconSize: "20px",
+      text: "Telegram",
+      url: "https://t.me/zitcoinL2",
     },
   ];
 
   const frontendLink = (() => {
     if (config.UI.footer.frontendVersion) {
-      return <Link href={ FRONT_VERSION_URL } target="_blank">{ config.UI.footer.frontendVersion }</Link>;
+      return (
+        <Link href={FRONT_VERSION_URL} target="_blank">
+          {config.UI.footer.frontendVersion}
+        </Link>
+      );
     }
 
     if (config.UI.footer.frontendCommit) {
-      return <Link href={ FRONT_COMMIT_URL } target="_blank">{ config.UI.footer.frontendCommit }</Link>;
+      return (
+        <Link href={FRONT_COMMIT_URL} target="_blank">
+          {config.UI.footer.frontendCommit}
+        </Link>
+      );
     }
 
     return null;
@@ -96,98 +111,133 @@ const Footer = () => {
 
   const fetch = useFetch();
 
-  const { isPlaceholderData, data: linksData } = useQuery<unknown, ResourceError<unknown>, Array<CustomLinksGroup>>({
-    queryKey: [ 'footer-links' ],
-    queryFn: async() => fetch(config.UI.footer.links || '', undefined, { resource: 'footer-links' }),
+  const { isPlaceholderData, data: linksData } = useQuery<
+    unknown,
+    ResourceError<unknown>,
+    Array<CustomLinksGroup>
+  >({
+    queryKey: ["footer-links"],
+    queryFn: async () =>
+      fetch(config.UI.footer.links || "", undefined, {
+        resource: "footer-links",
+      }),
     enabled: Boolean(config.UI.footer.links),
     staleTime: Infinity,
     placeholderData: [],
   });
 
-  const colNum = isPlaceholderData ? 1 : Math.min(linksData?.length || Infinity, MAX_LINKS_COLUMNS) + 1;
+  const colNum = isPlaceholderData
+    ? 1
+    : Math.min(linksData?.length || Infinity, MAX_LINKS_COLUMNS) + 1;
 
-  const renderNetworkInfo = React.useCallback((gridArea?: GridProps['gridArea']) => {
-    return (
-      <Flex
-        gridArea={ gridArea }
-        flexWrap="wrap"
-        columnGap={ 8 }
-        rowGap={ 6 }
-        mb={{ base: 5, lg: 10 }}
-        _empty={{ display: 'none' }}
-      >
-        { !config.UI.indexingAlert.intTxs.isHidden && <IntTxsIndexingStatus/> }
-        <NetworkAddToWallet/>
-      </Flex>
-    );
-  }, []);
+  const renderNetworkInfo = React.useCallback(
+    (gridArea?: GridProps["gridArea"]) => {
+      return (
+        <Flex
+          gridArea={gridArea}
+          flexWrap="wrap"
+          columnGap={8}
+          rowGap={6}
+          mb={{ base: 5, lg: 10 }}
+          _empty={{ display: "none" }}
+        >
+          {!config.UI.indexingAlert.intTxs.isHidden && <IntTxsIndexingStatus />}
+          <NetworkAddToWallet />
+        </Flex>
+      );
+    },
+    []
+  );
 
-  const renderProjectInfo = React.useCallback((gridArea?: GridProps['gridArea']) => {
-    return (
-      <Box gridArea={ gridArea }>
-        <Link fontSize="xs" href="https://ztcscan.com">ztcscan.com</Link>
-        <Text mt={ 3 } fontSize="xs">
-        Zitcoin utilizes Bitcoin's native addresses, UTXO client verification, and integrates with Bitcoin's Layer 2 network through Taproot Consensus.
-        </Text>
-        <VStack spacing={ 1 } mt={ 6 } alignItems="start">
-          { apiVersionUrl && (
-            <Text fontSize="xs">
-              Backend: <Link href={ apiVersionUrl } target="_blank">{ backendVersionData?.backend_version }</Link>
-            </Text>
-          ) }
-          { frontendLink && (
-            <Text fontSize="xs">
-              Frontend: { frontendLink }
-            </Text>
-          ) }
-        </VStack>
-      </Box>
-    );
-  }, [ apiVersionUrl, backendVersionData?.backend_version, frontendLink ]);
+  const renderProjectInfo = React.useCallback(
+    (gridArea?: GridProps["gridArea"]) => {
+      return (
+        <Box gridArea={gridArea}>
+          <Link fontSize="xs" href="https://ztcscan.com">
+            ztcscan.com
+          </Link>
+          <Text mt={3} fontSize="xs">
+            Zitcoin utilizes Bitcoin's native addresses, UTXO client
+            verification, and integrates with Bitcoin's Layer 2 network through
+            Taproot Consensus.
+          </Text>
+          <VStack spacing={1} mt={6} alignItems="start">
+            {apiVersionUrl && (
+              <Text fontSize="xs">
+                Backend:{" "}
+                <Link href={apiVersionUrl} target="_blank">
+                  {backendVersionData?.backend_version}
+                </Link>
+              </Text>
+            )}
+            {frontendLink && (
+              <Text fontSize="xs">Frontend: {frontendLink}</Text>
+            )}
+          </VStack>
+        </Box>
+      );
+    },
+    [apiVersionUrl, backendVersionData?.backend_version, frontendLink]
+  );
 
   const containerProps: GridProps = {
-    as: 'footer',
+    as: "footer",
     px: { base: 4, lg: 12 },
     py: { base: 4, lg: 9 },
-    borderTop: '1px solid',
-    borderColor: 'divider',
-    gridTemplateColumns: { base: '1fr', lg: 'minmax(auto, 470px) 1fr' },
-    columnGap: { lg: '32px', xl: '100px' },
+    borderTop: "1px solid",
+    borderColor: "divider",
+    gridTemplateColumns: { base: "1fr", lg: "minmax(auto, 470px) 1fr" },
+    columnGap: { lg: "32px", xl: "100px" },
   };
 
   if (config.UI.footer.links) {
     return (
-      <Grid { ...containerProps }>
+      <Grid {...containerProps}>
         <div>
-          { renderNetworkInfo() }
-          { renderProjectInfo() }
+          {renderNetworkInfo()}
+          {renderProjectInfo()}
         </div>
 
         <Grid
-          gap={{ base: 6, lg: colNum === MAX_LINKS_COLUMNS + 1 ? 2 : 8, xl: 12 }}
-          gridTemplateColumns={{
-            base: 'repeat(auto-fill, 160px)',
-            lg: `repeat(${ colNum }, 135px)`,
-            xl: `repeat(${ colNum }, 160px)`,
+          gap={{
+            base: 6,
+            lg: colNum === MAX_LINKS_COLUMNS + 1 ? 2 : 8,
+            xl: 12,
           }}
-          justifyContent={{ lg: 'flex-end' }}
+          gridTemplateColumns={{
+            base: "repeat(auto-fill, 160px)",
+            lg: `repeat(${colNum}, 135px)`,
+            xl: `repeat(${colNum}, 160px)`,
+          }}
+          justifyContent={{ lg: "flex-end" }}
           mt={{ base: 8, lg: 0 }}
         >
-          {
-            ([
-              { title: 'Blockscout', links: BLOCKSCOUT_LINKS },
-              ...(linksData || []),
-            ])
-              .slice(0, colNum)
-              .map(linkGroup => (
-                <Box key={ linkGroup.title }>
-                  <Skeleton fontWeight={ 500 } mb={ 3 } display="inline-block" isLoaded={ !isPlaceholderData }>{ linkGroup.title }</Skeleton>
-                  <VStack spacing={ 1 } alignItems="start">
-                    { linkGroup.links.map(link => <FooterLinkItem { ...link } key={ link.text } isLoading={ isPlaceholderData }/>) }
-                  </VStack>
-                </Box>
-              ))
-          }
+          {[
+            { title: "Blockscout", links: BLOCKSCOUT_LINKS },
+            ...(linksData || []),
+          ]
+            .slice(0, colNum)
+            .map((linkGroup) => (
+              <Box key={linkGroup.title}>
+                <Skeleton
+                  fontWeight={500}
+                  mb={3}
+                  display="inline-block"
+                  isLoaded={!isPlaceholderData}
+                >
+                  {linkGroup.title}
+                </Skeleton>
+                <VStack spacing={1} alignItems="start">
+                  {linkGroup.links.map((link) => (
+                    <FooterLinkItem
+                      {...link}
+                      key={link.text}
+                      isLoading={isPlaceholderData}
+                    />
+                  ))}
+                </VStack>
+              </Box>
+            ))}
         </Grid>
       </Grid>
     );
@@ -195,7 +245,7 @@ const Footer = () => {
 
   return (
     <Grid
-      { ...containerProps }
+      {...containerProps}
       gridTemplateAreas={{
         lg: `
           "network links-top"
@@ -203,29 +253,30 @@ const Footer = () => {
         `,
       }}
     >
-
-      { renderNetworkInfo({ lg: 'network' }) }
-      { renderProjectInfo({ lg: 'info' }) }
+      {renderNetworkInfo({ lg: "network" })}
+      {renderProjectInfo({ lg: "info" })}
 
       <Grid
-        gridArea={{ lg: 'links-bottom' }}
-        gap={ 1 }
+        gridArea={{ lg: "links-bottom" }}
+        gap={1}
         gridTemplateColumns={{
-          base: 'repeat(auto-fill, 160px)',
-          lg: 'repeat(3, 160px)',
-          xl: 'repeat(4, 160px)',
+          base: "repeat(auto-fill, 160px)",
+          lg: "repeat(3, 160px)",
+          xl: "repeat(4, 160px)",
         }}
         gridTemplateRows={{
-          base: 'auto',
-          lg: 'repeat(3, auto)',
-          xl: 'repeat(2, auto)',
+          base: "auto",
+          lg: "repeat(3, auto)",
+          xl: "repeat(2, auto)",
         }}
-        gridAutoFlow={{ base: 'row', lg: 'column' }}
+        gridAutoFlow={{ base: "row", lg: "column" }}
         alignContent="start"
-        justifyContent={{ lg: 'flex-end' }}
+        justifyContent={{ lg: "flex-end" }}
         mt={{ base: 8, lg: 0 }}
       >
-        { BLOCKSCOUT_LINKS.map(link => <FooterLinkItem { ...link } key={ link.text }/>) }
+        {BLOCKSCOUT_LINKS.map((link) => (
+          <FooterLinkItem {...link} key={link.text} />
+        ))}
       </Grid>
     </Grid>
   );
